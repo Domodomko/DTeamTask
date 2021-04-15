4df7e248108bc4405d27d364f137c86d7ae17e8b
Domodom <viablochko@gmail.com>
2021-04-15 20:23:57 +0600

    Made News author field nullable;

--- DTeamTask/news/models.py
+++ DTeamTask/news/models.py
@@ -6,7 +6,7 @@ class News(models.Model):
     title = models.CharField('Title', max_length=100, default='')
     content = models.TextField('Content', max_length=1000, default='')
     publication_date = models.DateField('Publication Date', auto_now_add=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news', null=True, blank=True)
 
     def __str__(self):
         return self.title
