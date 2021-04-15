fad19a4e8b1b213a41489e9c3515cb6bfa331d7a
Domodom <viablochko@gmail.com>
2021-04-16 02:48:58 +0600

    Created HelloWorldMiddleware
    Created HelloWorldMiddlware. It creates HTML comment and a header;

--- DTeamTask/DTeamTask/settings.py
+++ DTeamTask/DTeamTask/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'utils.middleware.HelloWorldMiddleware'
 ]
 
 ROOT_URLCONF = 'DTeamTask.urls'
---
+++ DTeamTask/utils/middleware.py
@@ -0,0 +1,11 @@
+
+class HelloWorldMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        comment = '<!--Hello, World!-->'
+        request.comment = comment
+        response = self.get_response(request)
+        response['Message to a World'] = "Hello, World!"
+        return response
