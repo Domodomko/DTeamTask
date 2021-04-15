e3381c66ff8d6d68c34fde3f350b9c37e96e7ca4
Domodom <viablochko@gmail.com>
2021-04-15 19:02:31 +0600

    Created news app
    Created "news" app;
    Created News model;
    Made migrations;
    Added news app to INSTALLED_APPS;
    Updated requirements.txt

---
+++ DTeamTask/news/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField('Title', max_length=100, default='')
+    content = models.CharField('Content', max_length=1000, default='')
+    publication_date = models.DateField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
