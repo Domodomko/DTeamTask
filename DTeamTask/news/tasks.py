005d97f47e9e46a6526bc62b870352df22d5f09b
Domodom <viablochko@gmail.com>
2021-04-16 13:24:07 +0600

    Updated create_new_news function
    Rewroted create_new_news function, now it fills the fileds with random words;

--- DTeamTask/news/tasks.py
+++ DTeamTask/news/tasks.py
@@ -2,11 +2,14 @@ from celery import shared_task
 from .models import News
 import random
 import string
+from random_word import RandomWords
+
+r = RandomWords()
 
 
 @shared_task
 def create_new_news():
-    random_title = ''.join([random.choice(string.ascii_letters) for _ in range(10)])
-    random_content = ''.join([random.choice(string.ascii_letters) for _ in range(100)])
+    random_title = ' '.join([r.get_random_word() for _ in range(5)])
+    random_content = ' '.join([r.get_random_word() for _ in range(70)])
     news = News.objects.create(title=random_title, content=random_content)
     return news.title
